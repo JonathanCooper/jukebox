9d1adcdc12874361f0a4c51ee1e9c28686119d01
jc <root@coopj.rollingsixes.us>
2013-08-01 16:06:20 -0400

    functions and functionality by jane austen

--- mplayer.php
+++ mplayer.php
@@ -1,40 +1,68 @@
 <?php
 
 include('includes/conf.php');
-if (!file_exists($fifo_path)) {
-	posix_mkfifo($fifo_path, 0664);
-}
-if (!exec("ps auwwx | grep 'mplayer -ao " . $sound_card . " -slave -idle -input file=".$fifo_path."' | grep -v grep", $out, $ret)) {
-	exec("mplayer -ao " . $sound_card . " -slave -idle -input file=".$fifo_path." >/dev/null &");
+
+function startup() {
+	if (!file_exists($fifo_path)) {
+		posix_mkfifo($fifo_path, 0664);
+	}
+	if (!exec("ps auwwx | grep 'mplayer -ao " . $sound_card . " -slave -idle -input file=".$fifo_path."' | grep -v grep", $out, $ret)) {
+		exec("mplayer -ao " . $sound_card . " -slave -idle -input file=".$fifo_path." >/dev/null &");
+	}
 }
 
-$id = intval($_POST['id']);
-$memcache = new Memcache;
-$memcache->connect('localhost');
 
-/*
+function play($start) {
+	$memcache = new Memcache;
+	$memcache->connect('localhost');
+
+	$current_list = unserialize($memcache->get('jukebox_latest'));
+
+	foreach ($current_list as $row) {
+		if ($row['id'] != $start) {
+			array_push($current_list, $row);
+			array_shift($current_list);
+		}
+		else {
+			break;
+		}
+	}
+
+	$fh = fopen("/tmp/mplayerplaylist", "w");
+	foreach ($current_list as $to_write) {
+		fwrite($fh, $to_write['fullpath']."\n");
+	}
+	fclose($fh);
+	file_put_contents("/tmp/mplayercontrol", "loadlist /tmp/mplayerplaylist\n");
+}
+
+function pause() {
+	file_put_contents("/tmp/mplayercontrol", "pause\n");
+}
+
+function skip($direction) {
+	if ($direction == 'forward') {
+		file_put_contents("/tmp/mplayercontrol", "pt_step +1\n");
+	} else {
+		file_put_contents("/tmp/mplayercontrol", "pt_step -1\n");
+	}
+}
+
+startup();
+
+if ($_POST['action'] == 'play') {
+	play(intval($_POST['param']));
+} elseif ($_POST['action'] == 'skip') {
+	//TODO: if ($_POST['param'] != 'forward' && $_POST['param' != 'back') {
+	skip($_POST['param']);
+} elseif ($_POST['action'] == 'pause') {
+	pause();
+} //TODO: else {
+	
+
+
 $fh = fopen('out.log', 'a');
-fwrite($fh, $_POST['id']);
+fwrite($fh, sprintf("%s | %s\n", $_POST['action'], $_POST['param']));
 fclose($fh);
-*/
-
-$current_list = unserialize($memcache->get('jukebox_latest'));
-
-foreach ($current_list as $row) {
-	if ($row['id'] != $id) {
-		array_push($current_list, $row);
-		array_shift($current_list);
-	}
-	else {
-		break;
-	}
-}
-
-$fh = fopen("/tmp/mplayerplaylist", "w");
-foreach ($current_list as $to_write) {
-	fwrite($fh, $to_write['fullpath']."\n");
-}
-fclose($fh);
-file_put_contents("/tmp/mplayercontrol", "loadlist /tmp/mplayerplaylist\n");
 
 ?>
